fd23d96946585572b90c453352e43679cd89ab75
sxjane <sxj.jane@gmail.com>
2021-01-17 20:29:56 +0100

    get one image by three ways

--- src/components/DisplayImage.jsx
+++ src/components/DisplayImage.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import axios from 'axios'
+import './styles.css'
 
 export default class DisplayImage extends React.Component{
     constructor(props){
         super(props)
-        this.state={src:null}
-        this.imgRef = React.createRef()
+        this.state={first:null,second:null}
     }
 
     onClick(){
-        axios.get('/base64_data')
-             .then((response)=>{
-                const img_data = response.data.img 
-                const img_dataURL = 'data:image/jpeg;base64,' + img_data
-                this.setState({src:img_dataURL})
+        axios.get('/next').then((response)=>{
+                const first_id = response.data[0]._id
+                const first_chip = response.data[0].chip
+                const second_id = response.data[1]._id 
+                const second_chip = response.data[1].chip
+                const first_URL = 'data:image/jpeg;base64,' + first_chip
+                const second_URL = 'data:image/jpeg;base64,' + second_chip
+                this.setState({first:first_URL, second:second_URL})
              }).catch(err=>console.log(err))
     }
 
     onClick2(){
-        axios.get('/base64_data', { responseType: 'blob' })
+        axios.get('/images', { responseType: 'blob' })
         .then((response)=>{
            const img_data = response.data
            let reader = new FileReader();
@@ -31,7 +34,7 @@ export default class DisplayImage extends React.Component{
     }
 
     onClick3(){
-        axios.get('/base64_data', { responseType: 'blob'})
+        axios.get('/images', { responseType: 'blob'})
         .then((response)=>{
            const img_data = response.data
            const imgURL = URL.createObjectURL(response.data)
@@ -42,8 +45,9 @@ export default class DisplayImage extends React.Component{
     render(){
         return (
             <div>
-                <img src={this.state.src}/>
-                <button onClick={this.onClick2.bind(this)}>Get An Image</button>
+                <img src={this.state.first}/>
+                <img src={this.state.second} />
+                <button onClick={this.onClick.bind(this)}>Get Images</button>
             </div>
         )
     }
